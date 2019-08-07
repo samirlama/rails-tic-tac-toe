9628ca99110647e06dbbb121c05b3e6166dd3ece
samir <samirlama509@gmail.com>
2019-08-07 21:45:56 +0545

    hotfix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,10 @@ module ApplicationHelper
         @player = Player.find(name)
         return @player.username
     end
+    def player_count(player)
+        @player = Gameplayer.all.where(game_status: "win" )
+        @player_count = @player.where(player_id: player).count
+        return @player_count
+    end
     
 end
